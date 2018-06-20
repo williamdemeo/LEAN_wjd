47f877341a9b1f6d06d51fe4dd408dee690e9e43
williamdemeo <williamdemeo@gmail.com>
2018-06-19 23:48:38 -0500

    essentially finishes ch 9

--- theorem_proving/src/09-structures_and_records.lean
+++ theorem_proving/src/09-structures_and_records.lean
@@ -1,12 +1,80 @@
--- 9. Structures and Records
+#print "Chapter 9. Structures and Records
+
+We've seen that Lean's foundational system includes inductive types. 
+
+We've noted the remarkable fact that it is possible to construct a substantial 
+edifice of math based on just the type universes, Pi types, and inductive types; 
+everything else follows from these. 
+
+Lean's std lib contains many instances of inductive types (e.g., nat, prod, list); 
+even the logical connectives are defined using inductive types.
+
+Remember, a non-recursive inductive type that contains only one constructor is called 
+a *structure* or *record*. The product type is a structure, as is the dependent product 
+type, that is, the *Sigma type*. 
+
+In general, whenever we define a structure `S`, we usually define *projection* functions 
+that allow us to 'destruct' each instance of `S` and retrieve the values stored in its 
+fields. The functions `prod.pr1` and `prod.pr2`---the first and second elements of a 
+pair---are examples.
+
+When writing programs or formalizing math, it's common to define structures containing 
+many fields. The `structure` command, available in Lean, provides infrastructure to 
+support this process. When we use this command, Lean automatically generates all projection 
+functions. 
+
+The `structure` command also allows us to define new structures based on previously defined 
+ones. Moreover, Lean provides convenient notation for instances of a given structure."
 
 
-#print "==========================================="
-#print "Section 9.1. Declaring Structures"
-#print " "
+#print "===========================================
+Section 9.1. Declaring Structures
 
+The structure command is essentially a 'front end' for defining inductive data types. 
+Every `structure` declaration introduces a namespace with the same name. 
+
+The general form is as follows:
+
+    structure <name> <parameters> <parent-structures> : Sort u :=
+      <constructor> :: <fields>
+
+Most parts are optional. Here is an example:"
 namespace Sec_9_1
+  structure point (α : Type) := mk :: (x: α) (y: α)
+  -- Values of type point are created by instantiating the point structure with 
+  -- the mk constructor, as follows:
+  #reduce point.mk 2 4
+  -- ...and here are the projections:
+  #reduce point.x (point.mk 2 4)  #reduce point.y (point.mk 2 4)
 
+  --The structure command also generates some useful recursors and theorems, e.g.,
+
+  -- point introduction
+  #check point           -- point: Type → Type
+
+  -- point elimination
+  #check @point.rec_on   --   Π {α: Type} {C: point α → Sort u_1} (p : point), 
+                         --     (Π (x y : α), C {x := x, y:= y}) → C p
+  -- the projections
+  #check @point.x         -- Π {α : Type}, point α → α                         
+  #check @point.y         -- Π {α : Type}, point α → α                         
+
+  -- you can obtain a complete list of the generated constructions using `#print prefix`
+  #print prefix Sec_9_1.point
+
+  -- Above we constructed a point and then took projections, like this:
+  #reduce point.mk 2 4  #reduce point.x (point.mk 2 4)  #reduce point.y (point.mk 2 4)
+  -- We could have opened point and then dropped "point." from each of these.
+  open point
+  #reduce mk 2 4
+  #reduce x (mk 2 4)  -- result: 2  ...seems a bit cryptic if you're not used to it
+  #reduce (mk 2 4).x  -- result: 2  ...using instead the . style like in oop
+  #reduce (mk 2 4).y  -- result: 4
+
+  example (α: Type) (a b : α) : x (mk a b) = a := rfl
+  theorem example_2nd_projection (α: Type) (a b : α) : y (mk a b) = b := rfl
+  #print example_2nd_projection
+  
 end Sec_9_1
 
 
--- theorem_proving/src/10-type_classes.lean
+++ theorem_proving/src/10-type_classes.lean
@@ -1,10 +1,48 @@
--- 10. Type Classes
+#print "Chapter 10. Type Classes
 
-#print "==========================================="
-#print "Section 10.1. Type Classes and Instances"
-#print " "
+We've seen that Lean's elaborator provides helpful automation, filling in information 
+that is tedious to enter by hand. In this section we'll explore a simple but powerful 
+technical device known as *type class inference*, which provides yet another mechanism 
+for the elaborator to supply missing information.
+
+*Type classes* originated with *Haskell*. In that context, type classes are often used 
+to associate operations, like a canonical addition or multiplication, to a data type. 
+This and other original uses carry over, but, as we will see, the realm of interactive 
+theorem proving raises even more possibilities for their use."
+
+
+#print "Section 10.1. Type Classes and Instances
+
+Any family of types can be marked as a *type class*. We can then declare particular 
+elements of a type class to be *instances*. These provide hints to the elaborator: 
+any time the elaborator is looking for an element of a type class, it can consult a 
+table of declared instances to find a suitable element.
+
+More precisely, there are three steps involved:
+
+1. declare a family of inductive types to be a type class;
+2. declare instances of the type class;
+3. mark some implicit arguments with square brackets instead of curly brackets 
+   to tell the elaborator to infer these arguments using the type class mechanism.
+
+We start with an example scenario. 
+
+Many theorems hold under the additional assumption that a type is inhabited, which 
+is to say, it has at least one element. For example, if `α` is a type, then
+`∃ x : α, x = x` holds iff `α` is inhabited. 
+
+Similarly, we often want a definition to return a default element in a 'corner case.' 
+For example, `head l` should be of type `α` when `l : list α`, but then we are faced 
+with the problem that `head l` needs to return an 'arbitrary' element of type `α` when
+`l` happens to be the empty list, `nil`.
+
+The std lib defines the type class `inhabited : Type → Type` to enable type class 
+inference to infer a 'default' or 'arbitrary' element of an inhabited type."
 
 namespace Sec_10_1
+-- First, we declare an appropriate class:
+
+
 
 end Sec_10_1
 
